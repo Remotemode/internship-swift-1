c18f2f78c0f7e3ac0e95e7230ba761de7a5eec84
Alexander Savchenko <alexandersav@wizardsdev.com>
2021-04-02 15:16:10 +0300

    initial commit. proj created.

---
+++ internship-swift/internship-swift/internship_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  internship_swiftApp.swift
+//  internship-swift
+//
+//  Created by Alexandr on 4/2/21.
+//
+
+import SwiftUI
+
+@main
+struct internship_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
