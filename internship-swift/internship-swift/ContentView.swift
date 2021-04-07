f3923f9a32a41adf626074dd8a9c7e29a87001f8
Alexander Savchenko <alexandersav@wizardsdev.com>
2021-04-07 16:20:46 +0300

    markup of list Item

--- internship-swift/internship-swift/ContentView.swift
+++ internship-swift/internship-swift/ContentView.swift
@@ -9,8 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing:0) {
+            Image(systemName: "captions.bubble")
+                .resizable()
+                .scaledToFit()
+                .frame(height: 180)
+                .font(Font.subheadline.weight(.thin))
+                .foregroundColor(.white)
+                .padding(.top, 5)
+            Text("Reddit user claiming to be Tesla insider appeared to reveal bitcoin buy a month ago ")
+                .font(Font.title2.weight(.bold))
+                .multilineTextAlignment(.center)
+                .foregroundColor(.white)
+                .frame(height:120)
+                .padding(.trailing, 20.0)
+                .padding(.leading, 20.0)
+        }
+        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 2.0)
+        .background(Color(.darkGray))
     }
 }
 
