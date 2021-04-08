c7101ba636bdec67eb762ebcbf9750dd50ecf78d
Alexander Savchenko <alexandersav@wizardsdev.com>
2021-04-08 16:09:12 +0300

    Add hardcoded list without the source. Modify appearance of Item.

--- internship-swift/internship-swift/ContentView.swift
+++ internship-swift/internship-swift/ContentView.swift
@@ -9,29 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing:0) {
+        List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+            ListItem(itemModel: .init())
+        }
+    }
+}
+
+struct ListItem: View {
+    let itemModel: ItemModel
+        
+    var body: some View {
+        VStack(alignment: .center, spacing:0) {
             Image(systemName: "captions.bubble")
                 .resizable()
-                .scaledToFit()
-                .frame(height: 180)
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 180.0)
                 .font(Font.subheadline.weight(.thin))
                 .foregroundColor(.white)
-                .padding(.top, 5)
-            Text("Reddit user claiming to be Tesla insider appeared to reveal bitcoin buy a month ago ")
+            Text(itemModel.title)
                 .font(Font.title2.weight(.bold))
                 .multilineTextAlignment(.center)
+                .frame(maxWidth: .infinity)
                 .foregroundColor(.white)
-                .frame(height:120)
                 .padding(.trailing, 20.0)
                 .padding(.leading, 20.0)
+                .lineLimit(4)
         }
         .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 2.0)
         .background(Color(.darkGray))
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 8")
     }
+}
 }
--- internship-swift/internship-swift/ItemModel.swift
+++ internship-swift/internship-swift/ItemModel.swift
@@ -12,7 +12,7 @@ struct ItemModel{
     let imageUrl: String
     let title: String
     
-    init(description: String = "description", expertComment: String = "comment", id: Int, imageUrl: String, title: String = "title") {
+    init(description: String = "description", expertComment: String = "comment", id: Int = 0, imageUrl: String = "", title: String = "title") {
         self.description = description
         self.expertComment = expertComment
         self.id = id
