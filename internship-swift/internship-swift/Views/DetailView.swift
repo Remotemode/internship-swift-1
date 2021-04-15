cc9100bf263fad5a9832c462ae4e0d83501bc359
Alexander Savchenko <alexandersav@wizardsdev.com>
2021-04-15 18:08:11 +0300

    markup of detail page

--- internship-swift/internship-swift/Views/DetailView.swift
+++ internship-swift/internship-swift/Views/DetailView.swift
@@ -9,20 +9,74 @@ import SwiftUI
 
 struct DetailView: View {
     let item: ItemModel
+    var size: CGFloat?
+    
     var body: some View {
-        VStack {
-            Text(item.title)
-            ItemImage()
-                .background(Color.black)
-            Text(item.description)
-            Text(item.expertComment)
+        ScrollView(.vertical, showsIndicators: true) {
+            VStack() {
+                DetailViewTitle(item: item)
+                ItemImage()
+                    .background(Color.black)
+                DetailViewDescription(item: item)
+                DetailViewExpertComment(item: item)
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
         DetailView(item: .init())
+    }
+}
+
+struct DetailViewTitle: View {
+    let item: ItemModel
+    
+    var body: some View {
+        HStack {
+            Text(item.title)
+                .font(.title)
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                .foregroundColor(.blue)
+                .multilineTextAlignment(.leading)
+            Spacer(minLength: 0)
+        }
+    }
+}
+
+struct DetailViewDescription: View {
+    let item: ItemModel
+    
+    var body: some View {
+        HStack {
+            Text(item.description)
+                .font(.body)
+                .foregroundColor(.blue)
+                .multilineTextAlignment(.leading)
+            Spacer(minLength: 0)
+        }
+    }
+}
+
+struct DetailViewExpertComment: View {
+    let item: ItemModel
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text(
+                    """
+                    Experts Comment
+
+                    \(item.expertComment)
+                    """
+                )
+                Spacer()
+            }
+            .padding()
+        }
+        .background(Color(.blue))
     }
 }
